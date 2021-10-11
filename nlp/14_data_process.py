c86409227709a65a6063e26d7a9b93eebe54988d
Samael <chc199310@gmail.com>
2021-10-11 20:41:31 +0800

    update

--- nlp/14_data_process.py
+++ nlp/14_data_process.py
@@ -1,4 +1,5 @@
 # %%
+import collections
 import numpy as np
 from torch.nn.modules import conv
 from torch.nn.modules.linear import Linear
@@ -17,4 +18,73 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from tqdm import tqdm
 import os
+import re
+# %%
+
+
+def read_time_machine():
+    with open('../data/TimeMachine/timemachine.txt', 'r') as f:
+        lines = f.readlines()
+    return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+
+
+lines = read_time_machine()
+# %%
+
+
+def tokenize(lines, token='word'):
+    if token == 'word':
+        return [line.split() for line in lines]
+    elif token == 'char':
+        return [list(line) for line in lines]
+    else:
+        print('Unknow type: ' + token)
+
+
+tokens = tokenize(lines)
+# %%
+
+
+class Vocab:
+    def __init__(self, tokens=None, min_freq=1, reserved_token=None) -> None:
+        if tokens is None:
+            tokens = []
+        if reserved_token is None:
+            reserved_token = []
+        counter = self.count_corpus(tokens)
+        self.token_freqs = sorted(counter.items(), key=lambda x: x[1],
+                                  reverse=True)
+        self.unk, uniq_tokens = 0, ['<UNK>'] + reserved_token
+        uniq_tokens += [token for token, freq in self.token_freqs
+                        if freq >= min_freq and token not in uniq_tokens]
+        self.idx_to_token, self.token_to_idx = [], dict()
+        for token in uniq_tokens:
+            self.idx_to_token.append(token)
+            self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[idx] for idx in indices]
+
+    def count_corpus(self, tokens):
+        if len(tokens) == 0 or isinstance(tokens[0], list):
+            tokens = [token for line in tokens for token in line]
+        return collections.Counter(tokens)
+
+
+# %%
+vocab = Vocab(tokens)
+print(list(vocab.token_to_idx.items())[:10])
+# %%
+print(tokens[0])
+print(vocab[tokens[0]])
 # %%
