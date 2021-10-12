8ec5f507b0d2361cb573b37574a6e5cb4f3b8268
samaelchen <chc199310@gmail.com>
2021-10-12 17:26:06 +0800

    update

---
+++ nlp/15_language_model.py
@@ -0,0 +1,121 @@
+# %%
+import collections
+import numpy as np
+from scipy.sparse.construct import rand
+from torch.nn.modules import conv
+from torch.nn.modules.linear import Linear
+from torch.nn.modules.pooling import MaxPool2d
+from torchsummary import summary
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils import data
+from torchvision import transforms
+from torchvision import datasets
+from torchvision import utils
+import matplotlib.pyplot as plt
+import torchvision
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from tqdm import tqdm
+import os
+import re
+import random
+# %%
+
+
+def read_time_machine():
+    with open('../data/TimeMachine/timemachine.txt', 'r') as f:
+        lines = f.readlines()
+    return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+
+
+# %%
+
+
+def tokenize(lines, token='word'):
+    if token == 'word':
+        return [line.split() for line in lines]
+    elif token == 'char':
+        return [list(line) for line in lines]
+    else:
+        print('Unknow type: ' + token)
+
+
+# %%
+
+
+class Vocab:
+    def __init__(self, tokens=None, min_freq=1, reserved_token=None) -> None:
+        if tokens is None:
+            tokens = []
+        if reserved_token is None:
+            reserved_token = []
+        counter = self.count_corpus(tokens)
+        self.token_freqs = sorted(counter.items(), key=lambda x: x[1],
+                                  reverse=True)
+        self.unk, uniq_tokens = 0, ['<UNK>'] + reserved_token
+        uniq_tokens += [token for token, freq in self.token_freqs
+                        if freq >= min_freq and token not in uniq_tokens]
+        self.idx_to_token, self.token_to_idx = [], dict()
+        for token in uniq_tokens:
+            self.idx_to_token.append(token)
+            self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[idx] for idx in indices]
+
+    def count_corpus(self, tokens):
+        if len(tokens) == 0 or isinstance(tokens[0], list):
+            tokens = [token for line in tokens for token in line]
+        return collections.Counter(tokens)
+
+
+# %%
+def seq_data_iter_random(corpus, batch_size, num_steps):
+    corpus = corpus[random.randint(0, num_steps-1):]
+    num_subseqs = (len(corpus) - 1) // num_steps
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+    random.shuffle(initial_indices)
+
+    def data(pos):
+        return corpus[pos: pos+num_steps]
+    num_batches = num_subseqs // batch_size
+    for i in range(0, batch_size*num_batches, batch_size):
+        initial_indices_per_batch = initial_indices[i:i+batch_size]
+        X = [data(j) for j in initial_indices_per_batch]
+        Y = [data(j+1) for j in initial_indices_per_batch]
+        yield torch.tensor(X), torch.tensor(Y)
+
+
+# %%
+lines = read_time_machine()
+tokens = tokenize(lines)
+corpus = [token for line in tokens for token in line]
+vocab = Vocab(tokens)
+print(list(vocab.token_to_idx.items())[:10])
+# %%
+print(tokens[0])
+print(vocab[tokens[0]])
+# %%
+vocab.token_freqs[:10]
+# %%
+bigram_tokens = [pair for pair in zip(corpus[:-1], corpus[1:])]
+bigram_vocab = Vocab(bigram_tokens)
+bigram_vocab.token_freqs[:10]
+# %%
+trigram_tokens = [triple for triple in zip(
+    corpus[:-2], corpus[1:-1], corpus[2:])]
+trigram_vocab = Vocab(trigram_tokens)
+trigram_vocab.token_freqs[:10]
+# %%
