743f611753cf78b0afba09dd5a4b2ffe420ae5f9
samaelchen <chc199310@gmail.com>
2021-10-20 16:48:57 +0800

    update

---
+++ nlp/18_lstm.py
@@ -0,0 +1,96 @@
+# %%
+import collections
+import numpy as np
+from scipy.sparse.construct import rand
+from torch.nn.modules import conv
+from torch.nn.modules.linear import Linear
+from torch.nn.modules.pooling import MaxPool2d
+from torchsummary import summary
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils import data
+from torchvision import transforms
+from torchvision import datasets
+from torchvision import utils
+import matplotlib.pyplot as plt
+import torchvision
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from tqdm import tqdm
+import os
+import re
+import random
+from d2l import torch as d2l
+from utils import load_data_time_machine
+# %%
+batch_size, num_steps = 32, 35
+train_iter, vocab = load_data_time_machine(
+    batch_size, num_steps, token_type='char')
+# %%
+
+
+def get_params(vocab_size, num_hiddens, device):
+    num_inputs = num_outputs = vocab_size
+
+    def normal(shape):
+        return torch.randn(size=shape, device=device) * 0.01
+
+    def three():
+        return(normal((num_inputs, num_hiddens)),
+               normal((num_hiddens, num_hiddens)),
+               torch.zeros(num_hiddens, device=device))
+
+    W_xz, W_hz, b_z = three()
+    W_xr, W_hr, b_r = three()
+    W_xh, W_hh, b_h = three()
+
+    W_hq = normal((num_hiddens, num_outputs))
+    b_q = torch.zeros(num_outputs, device=device)
+
+    params = [W_xz, W_hz, b_z, W_xr, W_hr, b_r, W_xh, W_hh, b_h, W_hq, b_q]
+    for param in params:
+        param.requires_grad_(True)
+    return params
+# %%
+
+
+class RNNModel(nn.Module):
+    def __init__(self, rnn_layer, vocab_size, **kwargs):
+        super(RNNModel, self).__init__(**kwargs)
+        self.rnn = rnn_layer
+        self.vocab_size = vocab_size
+        self.num_hiddens = self.rnn.hidden_size
+        if not self.rnn.bidirectional:
+            self.num_directions = 1
+            self.linear = nn.Linear(self.num_hiddens, self.vocab_size)
+        else:
+            self.num_directions = 2
+            self.linear = nn.Linear(self.num_hiddens * 2, self.vocab_size)
+
+    def forward(self, inputs, state):
+        X = F.one_hot(inputs.T.long(), self.vocab_size)
+        X = X.to(torch.float32)
+        Y, state = self.rnn(X, state)
+        output = self.linear(Y.reshape((-1, Y.shape[-1])))
+        return output, state
+
+    def begin_state(self, device, batch_size=1):
+        if not isinstance(self.rnn, nn.LSTM):
+            return torch.zeros((self.num_directions * self.rnn.num_layers,
+                                batch_size, self.num_hiddens), device=device)
+        else:
+            return (torch.zeros((self.num_directions * self.rnn.num_layers,
+                                 batch_size, self.num_hiddens), device=device),
+                    torch.zeros((self.num_directions * self.rnn.num_layers,
+                                 batch_size, self.num_hiddens), device=device))
+
+
+# %%
+vocab_size, num_hiddens, device = len(vocab), 256, d2l.try_gpu()
+epochs, lr = 500, 1
+num_inputs = vocab_size
+gru_layer = nn.LSTM(num_inputs, num_hiddens)
+model = RNNModel(gru_layer, len(vocab))
+d2l.train_ch8(model, train_iter, vocab, lr, epochs, device)
+# %%
