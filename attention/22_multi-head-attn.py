5eb6c55c948d1ef91e2aa99eac7850cce290f1de
samaelchen <chc199310@gmail.com>
2021-11-25 20:28:51 +0800

    update

--- attention/22_multi-head-attn.py
+++ attention/22_multi-head-attn.py
@@ -68,6 +68,67 @@ class DotProductAttention(nn.Module):
         self.attention_weights = masked_softmax(scores, valid_lens)
         return torch.bmm(self.dropout(self.attention_weights), values)
 
+
+# %%
+class MultiHeadAttention(nn.Module):
+    def __init__(self, key_size, query_size, value_size, num_hiddens,
+                 num_heads, dropout, bias=False, **kwargs):
+        super(MultiHeadAttention, self).__init__(**kwargs)
+        self.num_heads = num_heads
+        self.attention = d2l.DotProductAttention(dropout)
+        self.W_q = nn.Linear(query_size, num_hiddens, bias=bias)
+        self.W_k = nn.Linear(key_size, num_hiddens, bias=bias)
+        self.W_v = nn.Linear(value_size, num_hiddens, bias=bias)
+        self.W_o = nn.Linear(num_hiddens, num_hiddens, bias=bias)
+
+    def forward(self, queries, keys, values, valid_lens):
+        # `queries`, `keys`, or `values` 的形状:
+        # (`batch_size`, 查询或者“键－值”对的个数, `num_hiddens`)
+        # `valid_lens`　的形状:
+        # (`batch_size`,) or (`batch_size`, 查询的个数)
+        # 经过变换后，输出的 `queries`, `keys`, or `values`　的形状:
+        # (`batch_size` * `num_heads`, 查询或者“键－值”对的个数,
+        # `num_hiddens` / `num_heads`)
+        queries = transpose_qkv(self.W_q(queries), self.num_heads)
+        keys = transpose_qkv(self.W_k(keys), self.num_heads)
+        values = transpose_qkv(self.W_v(values), self.num_heads)
+
+        if valid_lens is not None:
+            # 在轴 0，将第一项（标量或者矢量）复制 `num_heads` 次，
+            # 然后如此复制第二项，然后诸如此类。
+            valid_lens = torch.repeat_interleave(
+                valid_lens, repeats=self.num_heads, dim=0)
+
+        # `output` 的形状: (`batch_size` * `num_heads`, 查询的个数,
+        # `num_hiddens` / `num_heads`)
+        output = self.attention(queries, keys, values, valid_lens)
+
+        # `output_concat` 的形状: (`batch_size`, 查询的个数, `num_hiddens`)
+        output_concat = transpose_output(output, self.num_heads)
+        return self.W_o(output_concat)
+
+
+def transpose_qkv(X, num_heads):
+    # 输入 `X` 的形状: (`batch_size`, 查询或者“键－值”对的个数, `num_hiddens`).
+    # 输出 `X` 的形状: (`batch_size`, 查询或者“键－值”对的个数, `num_heads`,
+    # `num_hiddens` / `num_heads`)
+    X = X.reshape(X.shape[0], X.shape[1], num_heads, -1)
+
+    # 输出 `X` 的形状: (`batch_size`, `num_heads`, 查询或者“键－值”对的个数,
+    # `num_hiddens` / `num_heads`)
+    X = X.permute(0, 2, 1, 3)
+
+    # `output` 的形状: (`batch_size` * `num_heads`, 查询或者“键－值”对的个数,
+    # `num_hiddens` / `num_heads`)
+    return X.reshape(-1, X.shape[2], X.shape[3])
+
+
+# @save
+def transpose_output(X, num_heads):
+    """逆转 `transpose_qkv` 函数的操作"""
+    X = X.reshape(-1, num_heads, X.shape[1], X.shape[2])
+    X = X.permute(0, 2, 1, 3)
+    return X.reshape(X.shape[0], X.shape[1], -1)
 # %%
 
 
@@ -76,7 +137,10 @@ class Seq2SeqAttnDecoder(nn.Module):
         super(Seq2SeqAttnDecoder, self).__init__(**kwargs)
         # self.attention = AdditiveAttention(
         #     num_hiddens, num_hiddens, num_hiddens, dropout)
-        self.attention = DotProductAttention(dropout=0.5)
+        # self.attention = DotProductAttention(dropout=0.5)
+        self.attention = MultiHeadAttention(num_hiddens, num_hiddens,
+                                            num_hiddens, num_hiddens,
+                                            num_heads=5, dropout=0.5)
         self.embedding = nn.Embedding(vocab_size, embed_size)
         self.rnn = nn.GRU(embed_size + num_hiddens,
                           num_hiddens, num_layers, dropout=dropout)
