71bb29ef30877aa7d515a35399bac680706ac559
samaelchen <chc199310@gmail.com>
2022-02-11 17:48:28 +0800

    update

--- extra/cosent/run.py
+++ extra/cosent/run.py
@@ -1,11 +1,12 @@
 # %%
 import os
+import sys
 import random
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from transformers import BertTokenizer, get_linear_schedule_with_warmup
 from model import CoSENT
 from utils import calc_corr, load_data, calc_cosim, cosentloss, CustomDataset
-from transformers import BertTokenizer, get_linear_schedule_with_warmup
 # %%
