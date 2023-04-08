2cf923ebefc0d1d10a52e5126257af4e83444e20
samaelchen <chc199310@gmail.com>
2023-04-08 14:10:41 +0800

    update

--- extra/wallhaven_dump.py
+++ extra/wallhaven_dump.py
@@ -8,15 +8,15 @@ wallhaven = Wallhaven(api_key='2RCqfqJ3p6MPRtIOLoKkfUxCHPFtprTI')
 wallhaven.params["categories"] = "001"
 wallhaven.params["purity"] = '001'
 wallhaven.params["sorting"] = "toplist"
-wallhaven.params["topRange"] = "1M"
-wallhaven.params["page"] = 1
-results = wallhaven.search()
+wallhaven.params["topRange"] = "1y"
 
-# %%
-for wallpaper in tqdm(results.data):
-    pic_info = wallhaven.get_wallpaper(wallpaper_id=wallpaper.id)
-    prompt = '\x01'.join([x.name for x in pic_info.tags])
-    with open('/home/samael/Pictures/SD_corpus/prompts.txt', 'a') as f:
-        f.write(prompt + '\x02' + pic_info.id + '\n')
-    wallpaper.save('/home/samael/Pictures/SD_corpus')
+for page in range(1, 381):
+    wallhaven.params["page"] = page
+    results = wallhaven.search()
+    for wallpaper in tqdm(results.data):
+        pic_info = wallhaven.get_wallpaper(wallpaper_id=wallpaper.id)
+        prompt = '\x01'.join([x.name for x in pic_info.tags])
+        with open('/home/samael/Pictures/SD_corpus/prompts.txt', 'a') as f:
+            f.write(prompt + '\x02' + pic_info.id + '\n')
+        wallpaper.save('/home/samael/Pictures/SD_corpus')
 # %%
